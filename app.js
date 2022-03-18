40b0bcc2212a8030f447cf2a95ec901afa205734
Daniel <ahmeddaniel18@gmail.com>
2022-03-18 00:14:52 -0400

    initial js funtions

---
+++ app.js
@@ -0,0 +1,41 @@
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const startBtn = document.querySelector('.btn__reset');
+const overlay = document.getElementById('overlay')
+let missed = 0;
+
+const phrases = [
+    "Cut To The Chase",
+    "Right Out of the Gate",
+    "Break The Ice",
+    "A Day Late and a Dollar Short",
+    "A Cut Below"
+];
+
+
+startBtn.addEventListener('click', () => {
+    overlay.style.display = 0;
+})
+
+function getRandomPhraseAsArray(arr) {
+
+    let randomNumber = [Math.floor(Math.random() * phrases.length)];
+    let randomPhrase = arr[randomNumber];
+
+    return randomPhrase.split();
+}
+
+function addPhraseToDisplay (arr){
+
+    for (let i = 0; i < arr.length; i++) {
+        let listItem = document.createElement('li');
+        listItem.innerHTML = arr[i];
+        document.getElementById("phrase, ul").appendChild(listItem);
+      }
+      
+    if (arr[i] === ' ') {
+        listItem.className = 'space';
+    } else {
+        listItem.className = 'letter';
+    }
+}
