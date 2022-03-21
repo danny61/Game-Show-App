e4485221398f90a5394f2eafeaf7d5f862749f18
Daniel <ahmeddaniel18@gmail.com>
2022-03-21 02:46:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -30,10 +30,12 @@ function addPhraseToDisplay (arr){
     const ul = phrase.querySelector("ul");
 
     for (let i = 0; i < arr.length; i++) {
+
         let listItem = document.createElement('li');
+
         listItem.innerHTML = arr[i];
+
         ul.appendChild(listItem);
-      
       
     if (arr[i] === ' ') {
         listItem.className = 'space';
